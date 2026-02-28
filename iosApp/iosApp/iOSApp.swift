2381e35bbdc32f0a5a6bddc55003837281dd3c68
raheem <raheemideadev@gmail.com>
2026-02-28 18:33:35 +0500

    Configured iOS for deep linking;

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,10 +1,14 @@
 import SwiftUI
+import ComposeApp
 
 @main
 struct iOSApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onOpenURL { URL in
+                    DeepLinkManager.shared.setUri(uri: URL.absoluteString)
+                }
         }
     }
 }
